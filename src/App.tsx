fcbcd265a2d3de3261160eedb5dcef645c7ace50
mdraselmiah95 <russelroy15@gmail.com>
2022-12-27 01:42:02 +0600

    evo gym typescript setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-green-400">
-      Hello world!
-    </h1>
+    <h1 className="bg-gray-20 text-3xl font-bold  underline">Hello world!</h1>
   );
 }
 
