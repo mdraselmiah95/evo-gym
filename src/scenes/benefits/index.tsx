abddd5f1afef971b39dfe7718af7a66a5c80a5e6
mdraselmiah95 <russelroy15@gmail.com>
2022-12-28 20:53:52 +0600

    new code added

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ActionButton from "@/shared/ActionButton";
+import { SelectedPage } from "@/shared/types";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return <div>Benefits</div>;
+};
+
+export default Benefits;
