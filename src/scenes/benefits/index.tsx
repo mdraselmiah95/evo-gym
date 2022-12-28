78bad083d112fb36befd8a14d5cbd882edaceda0
mdraselmiah95 <russelroy15@gmail.com>
2022-12-28 21:02:50 +0600

    WIP on evo GYM Benefits

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -8,13 +8,42 @@ import {
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
 import BenefitsPageGraphic from "@/assets/BenefitsPageGraphic.png";
+import HText from "@/shared/HText";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Benefits = ({ setSelectedPage }: Props) => {
-  return <div>Benefits</div>;
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>MORE THAN JUST GYM.</HText>
+          <p className="my-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
+        {/* BENEFITS */}
+        <motion.div></motion.div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Benefits;
