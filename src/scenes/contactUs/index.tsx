764ceef12932ebcca721244f8fe7ec02db2ec570
mdraselmiah95 <russelroy15@gmail.com>
2022-12-30 03:48:32 +0600

    WIP on evo gym Contact page

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -132,6 +132,25 @@ const ContactUs = ({ setSelectedPage }: Props) => {
               </button>
             </form>
           </motion.div>
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                className="w-full"
+                alt="contact-us-page-graphic"
+                src={ContactUsPageGraphic}
+              />
+            </div>
+          </motion.div>
         </div>
       </motion.div>
     </section>
