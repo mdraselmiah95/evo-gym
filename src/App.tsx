624ebc0cdb67a409bf0383a2682fec2f115add09
mdraselmiah95 <russelroy15@gmail.com>
2022-12-30 03:35:46 +0600

    WIP on evo gym Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -33,10 +34,11 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   );
 }
 
 export default App;
 
-//=> 2:50:17 This video will
+//=> 2:50:17 This video will  top
